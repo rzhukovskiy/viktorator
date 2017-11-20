40f44cc53729ff3bb18d719888beeeff8593a45c
rzhukovskiy <zhukovskey@gmail.com>
2017-11-20 18:39:19 +0300

    widget ver 0.5

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
     $('body').on('click','#addWidget', function(e) {
-        VK.callMethod('showAppWidgetPreviewBox', 'text', 'return {"title": "Цитата", "text": "Текст цитаты"};');
+        VK.callMethod(
+            'showAppWidgetPreviewBox',
+            'list',
+            'var u = API.users.get({"user_ids":Args.uid});' +
+            'var name = u[0].first_name + " " + u[0].last_name;' +
+            'return {"title": "' + $('#title').val() + '", ' +
+            '"rows": [{"title":name,"button":"узнать подробнее","button_url":"https://vk.com","text":"' + $('#text').val() + '", "icon_id":"id" + Args.uid}]};'
+        );
     });
 });
--- views/widget/index.php
+++ views/widget/index.php
@@ -1,1 +1,3 @@
+<input type="text" id="title" value="Привет,путник" /><br/>
+<input type="text" id="text" value="Lorem ipsum" /><br/>
 <a href="#" id="addWidget">Постановка виджета</a>
