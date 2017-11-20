4355d854543b026a5bc76a4279951448ab5de6a4
rzhukovskiy <zhukovskey@gmail.com>
2017-11-20 17:59:18 +0300

    widget second test

--- js/main.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-    $('body').on('click','#addWidget', function(e) {
-        VK.callMethod('showAppWidgetPreviewBox', 'text', 'return {"title": "Цитата", "text": "Текст цитаты"};');
-    });
-});
--- views/widget/index.php
+++ views/widget/index.php
@@ -1,1 +1,13 @@
-<a href="#" id="addWidget">жмак</a>
+<?php
+    $settings = 'VK.callMethod("users.get", {"user_ids": Args.uid}, function (data) {
+        return {
+            "title": "Привет, путник",
+            "rows": [{
+                "title": "title",
+                "text": "text",
+            }]
+        };
+    });';
+    $code = "VK.callMethod('showAppWidgetPreviewBox', 'text', '$settings')";
+?>
+<a href="#" onclick="<?= $code ?>">Постановка виджета</a>
