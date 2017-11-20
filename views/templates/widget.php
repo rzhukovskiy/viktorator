890645a1a31683f0de1fca67b54d706585e793e9
rzhukovskiy <zhukovskey@gmail.com>
2017-11-20 17:32:02 +0300

    links to js fixed

--- views/templates/widget.php
+++ views/templates/widget.php
@@ -33,7 +33,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="https://vk.com/js/api/xd_connection.js?2"  type="text/javascript"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="js/bootstrap.min.js"></script>
-<script src="js/main.js"></script>
+<script src="/js/bootstrap.min.js"></script>
+<script src="/js/main.js"></script>
 </body>
 </html>
