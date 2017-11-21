7a6f84b6d25cee79293306905412f19324dce80d
rzhukovskiy <zhukovskey@gmail.com>
2017-11-21 13:14:54 +0300

    widget view fixed

--- views/widget/index.php
+++ views/widget/index.php
@@ -1,5 +1,5 @@
 <input type="text" id="title" value="Привет,путник" /><br/>
 <input type="text" id="text" value="Основной текст. %username% понимает." /><br/>
 <input type="text" id="button_text" value="Текст кнопки" /><br/>
-<input type="text" id="button_url" value="Ссылка кнопки" /><br/>
+<input type="text" id="button_url" value="https://vk.com" /><br/>
 <a href="#" id="addWidget">Постановка виджета</a>
