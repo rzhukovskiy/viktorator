fe3e00cbe3e6de6110f2bda74817b660b72e7061
rzhukovskiy <zhukovskey@gmail.com>
2017-11-21 13:10:06 +0300

    widget js fixed again

--- js/main.js
+++ js/main.js
@@ -11,8 +11,7 @@ $(document).ready(function() {
             'var newText = text.split("%username%");' +
             'if(newText.length > 1) {text=""; var i=0; while(i < newText.length - 1) {text = text + newText[i] + name + newText[i+1]; i = i + 1;}};' +
             'return {"title": "' + title + '", ' +
-            '"rows": [{"title":name,"button":' + button_text +
-            ',"button_url":"' + button_url + '","text":"' + text + '", "icon_id":"id" + Args.uid}]};';
+            '"rows": [{"title":name,"button":"' + button_text + '","button_url":"' + button_url + '","text": text, "icon_id":"id" + Args.uid}]};';
 
         VK.callMethod('showAppWidgetPreviewBox', 'list', code);
     });
