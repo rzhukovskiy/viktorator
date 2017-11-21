df34c4ee2d97122c25cc052e13c605edee791e43
rzhukovskiy <zhukovskey@gmail.com>
2017-11-21 13:05:28 +0300

    widget js fixed

--- js/main.js
+++ js/main.js
@@ -5,17 +5,15 @@ $(document).ready(function() {
         var button_url  = $('#button_url').val();
         var button_text = $('#button_text').val();
 
-        VK.callMethod(
-            'showAppWidgetPreviewBox',
-            'list',
-            'var u = API.users.get({"user_ids":Args.uid});' +
+        var code = 'var u = API.users.get({"user_ids":Args.uid});' +
             'var name = u[0].first_name + " " + u[0].last_name;' +
             'var text = "' + text + '";' +
             'var newText = text.split("%username%");' +
-            'if (newText.length > 1){text = ""; var i = 0; while(i < newText.length - 1) {text = text + newText[i] + name + newText[i+1]; i = i + 1;}};'
+            'if(newText.length > 1) {text=""; var i=0; while(i < newText.length - 1) {text = text + newText[i] + name + newText[i+1]; i = i + 1;}};' +
             'return {"title": "' + title + '", ' +
             '"rows": [{"title":name,"button":' + button_text +
-            ',"button_url":"' + button_url + '","text":"' + text + '", "icon_id":"id" + Args.uid}]};'
-        );
+            ',"button_url":"' + button_url + '","text":"' + text + '", "icon_id":"id" + Args.uid}]};';
+
+        VK.callMethod('showAppWidgetPreviewBox', 'list', code);
     });
 });
