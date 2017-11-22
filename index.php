fdc3da8288f4f91af79ecb1a65ed1bb35a3f8f54
rzhukovskiy <zhukovskey@gmail.com>
2017-11-22 18:31:00 +0300

    first init fixed

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ if (!empty($argv)) {
 require_once(__DIR__ . '/app/Autoloader.php');
 spl_autoload_register(array('Autoloader', 'loadPackages'));
 
-Globals::init();
 BaseModel::init();
+Globals::init();
 $router = Router::getInstance();
 $router->handleRequest();
