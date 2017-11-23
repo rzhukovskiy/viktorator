9d625e398e1c086212f734dbdc09638b0c60ad33
rzhukovskiy <zhukovskey@gmail.com>
2017-11-23 16:49:00 +0300

    base entity get method fixed

--- app/BaseEntity.php
+++ app/BaseEntity.php
@@ -17,7 +17,11 @@ class BaseEntity
 
     public function __get($name)
     {
-        return $this->data[$name];
+        if (isset($this->data[$name])) {
+            return $this->data[$name];
+        } else {
+            return null;
+        }
     }
 
     public function __set($name, $value)
