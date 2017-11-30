fd928bc72520dacb099ccd01d307286a6ca2bf3b
rzhukovskiy <zhukovskey@gmail.com>
2017-11-30 15:57:02 +0300

    widget fixed

--- controllers/WidgetController.php
+++ controllers/WidgetController.php
@@ -8,25 +8,8 @@
  */
 class WidgetController extends BaseController
 {
-    /** @var $admin AdminEntity  */
-    private $admin = null;
-
-    public function init()
-    {
-        if ($_COOKIE['stoger']) {
-            $adminEntity = AdminModel::findBySocialId($_COOKIE['social_id']);
-
-            $this->admin = $adminEntity;
-        }
-
-        parent::init();
-    }
-
     public function actionIndex()
     {
-        if (!$this->admin) {
-            $this->template = 'widget';
-        }
         $this->render('widget/index');
     }
 }
