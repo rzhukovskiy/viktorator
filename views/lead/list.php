ba9d0115bca7f8f874a29ef7eac4f9b7525d5206
rzhukovskiy <zhukovskey@gmail.com>
2018-05-09 14:02:01 +0300

    fingerprint tests | lead list view fixed

--- views/lead/list.php
+++ views/lead/list.php
@@ -20,7 +20,7 @@
                     <tr>
                         <td><?= date("d-m-Y H:i", $lead->created_at + 3 * 3600) ?></td>
                         <td><?= $lead->ip?></td>
-                        <td><?= $lead->config?></td>
+                        <td><?= $lead->useragent?></td>
                         <td><?= $lead->hash?></td>
                     </tr>
                 <?php } ?>
